8fb8aa0b1abaa7f775100fecb34cd8f0455427a1
Gabriell Oliveira <gabrielloliveira097@gmail.com>
2021-10-25 20:07:57 -0300

    update process data test

--- retake/core/tests/conftest.py
+++ retake/core/tests/conftest.py
@@ -1,13 +1,25 @@
 import pytest
+from model_bakery import baker
 
 from retake.core.utils import ProcessData, PROCESS_01, PROCESS_02
 
 
 @pytest.fixture
-def process_01() -> ProcessData:
+def process_data_01() -> ProcessData:
     return ProcessData(PROCESS_01)
 
 
 @pytest.fixture
-def process_02() -> ProcessData:
+def process_data_02() -> ProcessData:
     return ProcessData(PROCESS_02)
+
+
+@pytest.fixture
+def process(db):
+    parts_set = baker.prepare("core.Part", _quantity=3)
+    return baker.make("core.Process", parts=parts_set)
+
+
+@pytest.fixture
+def part(db):
+    return baker.make("core.Part")
