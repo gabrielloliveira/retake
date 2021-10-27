92308ac3c89d65cb770190f869c41e4f8d38a6db
Gabriell Oliveira <gabrielloliveira097@gmail.com>
2021-10-26 21:25:20 -0300

    add docker support

--- retake/celery.py
+++ retake/celery.py
@@ -5,7 +5,7 @@ from celery.schedules import crontab
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'retake.settings')
 
-app = Celery('retake', broker='redis://localhost')
+app = Celery('retake', broker='redis://redis:6379')
 
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
